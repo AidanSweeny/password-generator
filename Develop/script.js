f145a07948dd5f5479dd874e4b65e7347462e626
Aidan Sweeny <apswee22@colby.edu>
2020-06-17 17:57:33 -0400

    Created variables

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,14 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p",
+"q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var numbers = [1,2,3,4,5,6,7,8,9,0]
+var spChars = ["+", "-", "&", "|", "!", "(", ")", "{", "}", "[", "]", "^",
+"~", "*", "?", ":","\"","\\"]
+var upLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var pswrd = [];
+var finalPswrd = [];
+
 
 // Write password to the #password input
 function writePassword() {
